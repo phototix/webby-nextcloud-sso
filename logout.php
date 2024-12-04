dbcce4186349ff6bb12c1c94d765f1fce49f8c40
Brandon Chong <phototix@hotmail.com>
2024-12-04 16:52:39 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -8,36 +8,6 @@ session_unset();
 // Destroy the session
 session_destroy();
 
-$config = include('../config.php');
-// Database connection details
-$host = $config['db_host'];
-$dbname = $config['db_name'];
-$username = $config['db_user'];
-$password = $config['db_pass'];
-
-try {
-    // Create PDO connection
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    
-    // Set PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // SQL to add new columns 'app_url' and 'cloud_url'
-    $sql = "ALTER TABLE db_admin 
-            ADD COLUMN app_url TEXT NOT NULL, 
-            ADD COLUMN cloud_url TEXT NOT NULL";
-    
-    // Execute the query
-    $pdo->exec($sql);
-    
-    echo "Columns 'app_url' and 'cloud_url' successfully added.";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-
-// Close the connection
-$pdo = null;
-
 // Redirect to a specific page after logout (e.g., homepage or login page)
 header("Location: /"); // Replace with your desired redirect URL
 exit;
