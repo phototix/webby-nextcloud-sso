c1b0e471746265999e20ed33d638d93099de510a
Brandon Chong <phototix@hotmail.com>
2025-03-25 16:54:45 +0800

    Update

--- oauth2/dashboard.php
+++ oauth2/dashboard.php
@@ -100,7 +100,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         function navigateTo(page) {
-            window.location.href = `/${page}.php`;
+            window.location.href = `/?page=grant&api=<?=$api?>&cate=${page}.php`;
         }
 
         function logout() {
